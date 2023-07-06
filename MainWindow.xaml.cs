ab5b8c80569bf55dac27900e81fd7bc2b2049755
SantosLeon11 <simulador27@gmail.com>
2023-07-06 09:24:59 -0500

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppPrueba
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGenR_Click(object sender, RoutedEventArgs e)
+        {
+            VentanaGenReserva interfaz = new();
+            interfaz.Show();
+           this.Close();
+        }
+
+        private void BtnConR_Click(object sender, RoutedEventArgs e)
+        {
+            VentanaConReserva interfaz = new ();
+            interfaz.Show();
+            this.Close();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            VentanaLogin interfaz1 = new();
+            interfaz1.Show();
+            this.Close();
+        }
+    }
+}
